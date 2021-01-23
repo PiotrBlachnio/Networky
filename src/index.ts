d4993934414600de004baeb91180a82598ef455b
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-23 15:53:23 +0100

    Create types/Context.ts

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,18 @@ import { RegisterResolver } from './modules/auth/register/Register';
 import { ErrorHandlerMiddleware } from './common/middlewares/ErrorHandlerMiddleware';
 import { Config } from './common/config';
 import { config } from 'dotenv';
+import { ConfigValidator } from './common/utils/ConfigValidator';
 
 config();
 
 const start = async () => {
+    await ConfigValidator.validate(Config);
     const schema = await buildSchema({ resolvers: [RegisterResolver] });
 
     const apolloServer = new ApolloServer({ schema, formatError: ErrorHandlerMiddleware });
-
     const app = express();
 
     apolloServer.applyMiddleware({ app });
-
     app.listen(Config.APP.PORT, () => console.log(`Server is running on port ${Config.APP.PORT}`));
 }
 
