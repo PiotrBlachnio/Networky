6c4b534c16c16b18d8bb97ae66f1219cae2f2454
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-24 10:27:46 +0100

    Remove type-graphql annotations from models/User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,5 @@
-import { Field, ID, ObjectType } from 'type-graphql';
-
-@ObjectType()
 export class User {
-    @Field(() => ID)
     id: string;
-
-    @Field()
     email: string;
-
     password: string;
 }
