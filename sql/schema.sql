64e37287f9f3c47aa62bf09037740b4d8bfd1a44
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-25 20:21:52 +0100

    Add more fields to the user model

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,4 +4,7 @@ CREATE TABLE "User" (
     id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v1(),
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
+    firstName TEXT NOT NULL,
+    lastName TEXT NOT NULL,
+    gender TEXT NOT NULL
 );
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,10 @@
+import { User as UserTypes } from '../common/constants/user';
+
 export class User {
     public readonly id: string;
     public readonly email: string;
     public readonly password: string;
+    public readonly firstName: string;
+    public readonly lastName: string;
+    public readonly gender: UserTypes.Gender;
 }
