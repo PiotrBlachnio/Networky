4f4869d2d53ee6c92d1b8f33335c76c75d40d57c
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-25 20:09:43 +0100

    Create sql/schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE "User" (
+    id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v1(),
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
