427e00b2ded047c1bd8c266f3e86321b9d7c99cf
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-29 14:15:17 +0100

    Create modules/post/dto/CreatePostRequest.ts

--- src/common/constants/dependency.ts
+++ src/common/constants/dependency.ts
@@ -1,8 +1,12 @@
 export enum Dependency {
     AUTH_SERVICE = 'auth_service',
     USER_SERVICE = 'user_service',
+    POST_SERVICE = 'post_service',
+    COMMENT_SERVICE = 'comment_service',
 
     USER_REPOSITORY = 'user_repository',
+    POST_REPOSITORY = 'post_repository',
+    COMMENT_REPOSITORY = 'comment_repository',
 
     TOKEN_SERVICE = 'token_service'
 }
---
+++ src/modules/post/PostService.ts
@@ -0,0 +1,16 @@
+import { Post } from '@prisma/client';
+import { inject, injectable } from 'inversify';
+import { Constants } from '../../common/constants';
+import { prisma } from '../../common/utils/Prisma';
+import { Context } from '../../types/Context';
+
+@injectable()
+export class PostService {
+    constructor(@inject(Constants.DEPENDENCY.POST_REPOSITORY) private readonly _postRepository: typeof prisma.post) {}
+
+    public async create(context: Context): Promise<Post> {
+        const post = await this._postRepository.create({ data: {
+            
+        }});
+    }
+}
