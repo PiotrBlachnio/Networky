48336775904128cb6227d39d6f8fe13b3d66d975
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-29 14:21:52 +0100

    Create modules/post/PostResolver.ts

---
+++ src/modules/post/PostResolver.ts
@@ -0,0 +1,19 @@
+import { inject, injectable } from 'inversify';
+import { Resolver, Authorized, Ctx, Mutation, Arg } from 'type-graphql';
+import { Constants } from '../../common/constants';
+import { Context } from '../../types/Context';
+import { CreatePostRequest } from './dto/CreatePostRequest';
+import { PostResponse } from './dto/PostResponse';
+import { PostService } from './PostService';
+
+@injectable()
+@Resolver()
+export class UserResolver {
+    constructor(@inject(Constants.DEPENDENCY.POST_SERVICE) private readonly _postService: PostService) {}
+
+    @Mutation(() => PostResponse)
+    @Authorized()
+    public async createPost(@Ctx() context: Context, @Arg('data') data: CreatePostRequest) {
+        return this._postService.create(context, data);
+    }
+}
